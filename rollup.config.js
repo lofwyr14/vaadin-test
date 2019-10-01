c2d7e096b70b49a187c768f294c84ce3a14cd936
Henning Noeth <hnoeth@apache.org>
2019-10-01 10:25:27 +0200

    implement rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import resolve from "rollup-plugin-node-resolve"
+
 export default {
   input: 'index.js',
   output: {
     file: 'bundle.js',
     format: 'iife',
     name: 'vaadin_test'
-  }
+  },
+  plugins: [
+    resolve()
+  ]
 };
